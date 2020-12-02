70f4409bef45a246513ab4e778e9f8f5cb01a663
frahman30 <frahman.iesl@gmail.com>
2020-12-02 19:01:03 +0500

    CUSTOM GYM APP CODE DEPLOYMENT (07:00 PM)

--- custom_gym_app/models/gym_registration.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import models, fields, api, _
-from odoo.exceptions import ValidationError
-
-
-class ResPartnerInheritance(models.Model):
-    _name = 'res.partner'
-    _inherit = 'res.partner'
